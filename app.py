99a7b7c2721079b214451e98de4df04572bd22ea
nitin bhojal <nitinbhojal@nitins-MacBook-Air.local>
2026-05-23 01:45:25 +0530

    changed message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "My First CI/CD Application"
+    return "My First CI/CD Application. added in git."
 
 if __name__ == '__main__':
     app.run(debug=True)
